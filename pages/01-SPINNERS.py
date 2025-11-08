727ed61def5ea0c3bb2cc889da632fb732d1d372
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2025-11-08 20:07:55 +0530

    Update and rename 01-SPINNERS.py to 03-SPINNERS.py

--- pages/01-SPINNERS.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.set_page_config(
-    page_title="BATTERS",
-    layout="wide"
-)
-st.title("Ye kal Hoga Jaan 😘")
---
+++ pages/03-SPINNERS.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(
+    page_title="SPINNERS",
+    layout="wide"
+)
+st.title("SPINNERS")
+st.header("Ye kal Hoga Jaan 😘")
