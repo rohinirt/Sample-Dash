cb7628ee2f654a20f7a72ffd567737135a214505
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2025-11-09 18:52:07 +0530

    Update 03-SPINNERS.py

--- pages/03-SPINNERS.py
+++ pages/03-SPINNERS.py
@@ -728,7 +728,7 @@ def create_spinner_hitting_missing_map(df_in, handedness_label):
 
     # 9️⃣ Remove any auto legends (safety)
     for legend in ax.get_children():
-        if isinstance(legend, plt.lLegend):
+        if isinstance(legend, plt.Legend):
             legend.remove()
 
     # 🔟 Add Hitting and Missing Text Labels
