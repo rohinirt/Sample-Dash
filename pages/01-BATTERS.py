d899783c1fc167290437c624d098e86a5cb6704a
Rohini Tembhurnikar <98411216+rohinirt@users.noreply.github.com>
2025-11-16 16:44:44 +0530

    Update 01-BATTERS.py

--- pages/01-BATTERS.py
+++ pages/01-BATTERS.py
@@ -815,8 +815,8 @@ def create_interception_side_on(df_in, delivery_type):
 
     # Draw the custom Rectangle 
     border_rect = patches.Rectangle(
-        (x0_pad-0.009, y0_pad+0.02), 
-        width_pad+0.025, 
+        (x0_pad-0.008, y0_pad+0.02), 
+        width_pad+0.02, 
         height_pad,  
         facecolor='none', 
         edgecolor='black', 
